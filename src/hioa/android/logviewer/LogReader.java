36dbddc7f430270b55fc69ddaf40eff85b9317d0
CheeseGnome <CheeseGnome@gmail.com>
2013-10-30 18:57:09 +0100

    LogReader

--- src/hioa/android/logviewer/LogReader.java
+++ src/hioa/android/logviewer/LogReader.java
@@ -4,19 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Custom implementation of BufferedReader designed to read system logs through
+ * the logcat command.
+ * <p>
+ * The class will self-initialize with an InputStreamReader on the inputstream
+ * of the logcat process.
+ * 
+ * @author Lars Sætaberget
+ * @version 2013-10-30
+ * @see BufferedReader
+ */
 public class LogReader extends BufferedReader {
 
 	private static final String[] COMMAND = { "logcat", "-d" };
-	
+
 	private static Process logcat_process;
 	static {
 		try {
 			logcat_process = Runtime.getRuntime().exec(COMMAND);
 		} catch (IOException ioe) {
-			//TODO do stuff
+			// TODO Exception handling
 		}
 	}
 
+	/**
+	 * Initializes a LogReader with an InputStreamReader on the inputstream of
+	 * the logcat process.
+	 * 
+	 * @see java.io.InputStream
+	 * @see InputStreamReader
+	 */
 	public LogReader() {
 		super(new InputStreamReader(logcat_process.getInputStream()));
 	}
