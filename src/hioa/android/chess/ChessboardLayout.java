f87d76f2e8571853c4e4da34e2b8575ba0a9c08d
CheeseGnome <CheeseGnome@gmail.com>
2013-11-15 17:45:36 +0100

    chessboardlayout.java

---
+++ src/hioa/android/chess/ChessboardLayout.java
@@ -0,0 +1,23 @@
+package hioa.android.chess;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TableLayout;
+
+/**
+ * A visual representation of a chessboard based on the chessboardlayout.xml file
+ * @author Lars Sætaberget
+ * @version 2013-11-15
+ */
+
+public class ChessboardLayout extends TableLayout {
+
+	public ChessboardLayout(Context context, AttributeSet attributes) {
+		super(context, attributes);
+		LayoutInflater layoutInflater = (LayoutInflater)context
+	              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    layoutInflater.inflate(R.layout.chessboardlayout, this);
+	}
+
+}
